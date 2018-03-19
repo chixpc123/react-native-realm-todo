5ce97c2b26ced6991e2de152cbe540e8fa701621
chirag lathiya <clathiya007@gmail.com>
2018-03-19 10:14:53 +0530

    Complete Todo App using realm

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import TodoListComponents from './components/TodoListComponents';
+
+AppRegistry.registerComponent('realmTodo', () => TodoListComponents);
